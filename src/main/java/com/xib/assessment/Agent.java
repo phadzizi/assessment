1c0d206f4eb49881349c8326a0885bf511c588e2
julios.hadzizi <julios.hadzizi@cgm.com>
2020-12-10 23:33:47 +0200

    Initial commit

---
+++ src/main/java/com/xib/assessment/Agent.java
@@ -0,0 +1,21 @@
+package com.xib.assessment;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+public class Agent {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    @ManyToOne
+    private Team team;
+}
---
+++ src/main/java/com/xib/assessment/config/Swagger2Config.java
@@ -0,0 +1,61 @@
+package com.xib.assessment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket api(SwaggerConfigProperties swaggerConfigProperties) {
+        return (new Docket( DocumentationType.SWAGGER_2 )).apiInfo( apiInfo( swaggerConfigProperties ) )
+                .enable( Boolean.parseBoolean( swaggerConfigProperties.getEnabled() ) )
+                .select()
+                .apis( RequestHandlerSelectors.withClassAnnotation( org.springframework.web.bind.annotation.RestController.class ) )
+                .paths( PathSelectors.any() )
+                .build()
+                .pathMapping( "/" )
+                .directModelSubstitute( LocalDate.class, String.class )
+                .genericModelSubstitutes( ResponseEntity.class )
+                .useDefaultResponseMessages( Boolean.parseBoolean( swaggerConfigProperties.getUseDefaultResponseMessages() ) )
+                .enableUrlTemplating( Boolean.parseBoolean( swaggerConfigProperties.getEnableUrlTemplating() ) );
+    }
+
+    @Bean
+    UiConfiguration uiConfig(SwaggerConfigProperties swaggerConfigProperties) {
+        return UiConfigurationBuilder.builder()
+                .operationsSorter( OperationsSorter.ALPHA )
+                .deepLinking( Boolean.valueOf( swaggerConfigProperties.getDeepLinking() ) )
+                .displayOperationId( Boolean.valueOf( swaggerConfigProperties.getDisplayOperationId() ) )
+                .defaultModelsExpandDepth( Integer.valueOf( swaggerConfigProperties.getDefaultModelsExpandDepth() ) )
+                .defaultModelExpandDepth( Integer.valueOf( swaggerConfigProperties.getDefaultModelExpandDepth() ) )
+                .defaultModelRendering( ModelRendering.EXAMPLE )
+                .displayRequestDuration( Boolean.valueOf( swaggerConfigProperties.getDisplayRequestDuration() ) )
+                .docExpansion( DocExpansion.NONE )
+                .filter( Boolean.valueOf( swaggerConfigProperties.getFilter() ) )
+                .maxDisplayedTags( Integer.valueOf( swaggerConfigProperties.getMaxDisplayedTags() ) )
+                .showExtensions( Boolean.valueOf( swaggerConfigProperties.getShowExtensions() ) )
+                .tagsSorter( TagsSorter.ALPHA )
+                .supportedSubmitMethods( UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS )
+                .validatorUrl( null )
+                .build();
+    }
+
+    private ApiInfo apiInfo(SwaggerConfigProperties swaggerConfigProperties) {
+        return new ApiInfoBuilder().title(swaggerConfigProperties.getTitle())
+                .description(swaggerConfigProperties.getDescription())
+                .version(swaggerConfigProperties.getApiVersion()).build();
+    }
+}
